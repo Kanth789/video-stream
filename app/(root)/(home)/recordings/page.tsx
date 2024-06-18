a6ac7635422c88e023ae81c6078f4593e5531bc2
Kiran Kanth Reddy <kanthkiran020@gmail.com>
2024-06-18 12:38:46 +0530

    commit

---
+++ app/(root)/(home)/recordings/page.tsx
@@ -0,0 +1,15 @@
+import CallList from '@/components/ui/CallList'
+import React from 'react'
+
+const Recording = () => {
+  return (
+    <section className="flex size-full flex-col gap-5 text-white">
+    <h1 className='text-3xl font-bold'>
+    Recording 
+    </h1>
+    <CallList type='recordings'/>
+   </section>
+  )
+}
+
+export default Recording
